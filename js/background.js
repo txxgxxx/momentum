ec96b19b98e090655e37d28d8198d74ba3c4f27e
txxgxxx <taegun1020@naver.com>
2023-11-12 00:08:38 +0900

    7.1 TO DO LIST Setup

--- js/background.js
+++ js/background.js
@@ -1,8 +1,10 @@
+const NATURE01 = "컴퓨터배경화면자연01.jpg"
+const NATURE02 = "컴퓨터배경화면자연02.jpg"
 const MINJI = "Minji.png";
 const KENDRICK = "kendrick.jpeg";
 const GREENPHOTO = "green.jpg";
 const images = [
-    MINJI, KENDRICK, GREENPHOTO
+    MINJI, KENDRICK, GREENPHOTO, NATURE01, NATURE02
 ]
 
 
@@ -12,5 +14,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.className = "bg"
 
-document.body.appendChild(bgImage);
+document.body.append(bgImage);
