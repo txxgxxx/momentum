1319b75d0d2ab3aab0a32a7510ce20dbd82ac4b3
txxgxxx <taegun1020@naver.com>
2023-11-13 00:38:12 +0900

    7.8 Deleting To Dos part Three

--- js/todo.js
+++ js/todo.js
@@ -12,8 +12,9 @@ function saveToDos(){
 
 function deleteToDo(event){
     const li = event.target.parentElement;
-    console.log(li.id);
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 
@@ -56,3 +57,4 @@ if(savedToDos){
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
 }
+
